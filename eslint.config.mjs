d3247cfda5092eecaed9af57eda54003241ef71b
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-07-04 14:29:36 +0200

    Update codestyle

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,44 +9,44 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    // Ignore patterns
-    {
-        ignores: ['**/*.md', '**/node_modules', '**/dist'],
-    },
+  // Ignore patterns
+  {
+    ignores: ['**/*.md', '**/node_modules', '**/dist'],
+  },
 
-    // Base JS + TS recommended configs
-    ...compat.extends(
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-    ),
+  // Base JS + TS recommended configs
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
 
-    // File-specific config for TypeScript
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                project: './tsconfig.test.json',
-                tsconfigRootDir: __dirname,
-                sourceType: 'module',
-            },
-        },
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-        },
-        rules: {
-            '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
-            '@typescript-eslint/explicit-function-return-type': 'error',
-            '@typescript-eslint/ban-ts-comment': 'error',
-            '@typescript-eslint/no-explicit-any': 'error',
-            '@typescript-eslint/no-non-null-assertion': 'error',
-        },
+  // File-specific config for TypeScript
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.test.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
     },
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', {argsIgnorePattern: '^_'}],
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+    },
+  },
 ];
--- tests/telegram-requests.test.ts
+++ tests/telegram-requests.test.ts
@@ -6,87 +6,87 @@ const CHAT_ID = process.env.TEST_CHAT_ID || '';
 const EXPECTED_USERNAME = process.env.TEST_BOT_USERNAME || '';
 
 describe('telegram requests integration tests', () => {
-    const sentMessageIds: number[] = [];
+  const sentMessageIds: number[] = [];
 
-    beforeAll(() => {
-        client_setClientToken(BOT_TOKEN);
-    });
+  beforeAll(() => {
+    client_setClientToken(BOT_TOKEN);
+  });
 
-    afterAll(
-        async () => {
-            let failed = false;
-            for (const messageId of sentMessageIds) {
-                try {
-                    await deleteMessage({
-                        chat_id: CHAT_ID,
-                        message_id: messageId,
-                    });
-                } catch (e) {
-                    failed = true;
-                    console.error('Failed to delete test message', e);
-                }
-            }
-
-            expect(failed).toBeFalsy();
-        },
-        40000);
-
-    test('should call a method with no args', async () => {
-        const response = await getMe({});
-        console.log(response);
-        expect(response).toBeDefined();
-        expect(response.ok).toBeTruthy();
-        expect(response.result.is_bot).toBeTruthy();
-        expect(response.result.username).toEqual(EXPECTED_USERNAME);
-    });
-
-    test('should call a method with simple args', async () => {
-        const randomStr = `Test message ${new Date().toISOString()}`;
-
-        const response = await sendMessage({
+  afterAll(
+    async () => {
+      let failed = false;
+      for (const messageId of sentMessageIds) {
+        try {
+          await deleteMessage({
             chat_id: CHAT_ID,
-            text: randomStr,
-        });
-        console.log(response);
-        expect(response).toBeDefined();
-        expect(response.ok).toBeTruthy();
-        expect(response.result.text).toEqual(randomStr);
+            message_id: messageId,
+          });
+        } catch (e) {
+          failed = true;
+          console.error('Failed to delete test message', e);
+        }
+      }
 
-        sentMessageIds.push(response.result.message_id);
+      expect(failed).toBeFalsy();
+    },
+    40000);
+
+  test('should call a method with no args', async () => {
+    const response = await getMe({});
+    console.log(response);
+    expect(response).toBeDefined();
+    expect(response.ok).toBeTruthy();
+    expect(response.result.is_bot).toBeTruthy();
+    expect(response.result.username).toEqual(EXPECTED_USERNAME);
+  });
+
+  test('should call a method with simple args', async () => {
+    const randomStr = `Test message ${new Date().toISOString()}`;
+
+    const response = await sendMessage({
+      chat_id: CHAT_ID,
+      text: randomStr,
     });
+    console.log(response);
+    expect(response).toBeDefined();
+    expect(response.ok).toBeTruthy();
+    expect(response.result.text).toEqual(randomStr);
 
-    test('should call a method with complex args', async () => {
-        const randomStr = `Test message ${new Date().toISOString()}`;
+    sentMessageIds.push(response.result.message_id);
+  });
 
-        const response = await sendMessage({
-            chat_id: CHAT_ID,
-            text: randomStr,
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        { text: 'Button 1', callback_data: '1' },
-                        { text: 'Button 2', callback_data: '2' },
-                    ],
-                ],
-            },
-        });
-        console.log(response);
-        expect(response).toBeDefined();
-        expect(response.ok).toBeTruthy();
-        expect(response.result.text).toEqual(randomStr);
+  test('should call a method with complex args', async () => {
+    const randomStr = `Test message ${new Date().toISOString()}`;
 
-        sentMessageIds.push(response.result.message_id);
+    const response = await sendMessage({
+      chat_id: CHAT_ID,
+      text: randomStr,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            { text: 'Button 1', callback_data: '1' },
+            { text: 'Button 2', callback_data: '2' },
+          ],
+        ],
+      },
     });
+    console.log(response);
+    expect(response).toBeDefined();
+    expect(response.ok).toBeTruthy();
+    expect(response.result.text).toEqual(randomStr);
 
-    test('should be able to send a photo by url', async () => {
-        const response = await sendPhoto({
-            chat_id: CHAT_ID,
-            photo: 'https://picsum.photos/200/300',
-        });
-        console.log(response);
-        expect(response).toBeDefined();
-        expect(response.ok).toBeTruthy();
+    sentMessageIds.push(response.result.message_id);
+  });
 
-        sentMessageIds.push(response.result.message_id);
+  test('should be able to send a photo by url', async () => {
+    const response = await sendPhoto({
+      chat_id: CHAT_ID,
+      photo: 'https://picsum.photos/200/300',
     });
+    console.log(response);
+    expect(response).toBeDefined();
+    expect(response.ok).toBeTruthy();
+
+    sentMessageIds.push(response.result.message_id);
+  });
 });
